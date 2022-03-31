2137c11ffc8fa584848dda99c1959c83b2ea4e95
Mike Penz <mikepenz@gmail.com>
2022-03-31 16:04:22 +0200

    - [release] v1.3.0-rc01

--- storyblok-mp-sdk/src/commonMain/kotlin/com/mikepenz/storyblok/sdk/Storyblok.kt
+++ storyblok-mp-sdk/src/commonMain/kotlin/com/mikepenz/storyblok/sdk/Storyblok.kt
@@ -302,7 +302,7 @@ class Storyblok constructor(
         private const val API_ENDPOINT = "api.storyblok.com"
         private const val API_VERSION = "v2"
 
-        private const val SDK_VERSION = "1.2.1"
+        private const val SDK_VERSION = "1.3.0-rc01"
         private const val SDK_USER_AGENT = "storyblok-sdk-android/$SDK_VERSION"
 
         private const val VERSION_PUBLISHED = "published"
